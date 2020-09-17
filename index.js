1116f0b833e88372d23c1d2591f8431f3db4558f
Oscar Bernal <oscarbernal93@gmail.com>
2020-09-17 10:29:52 -0500

    Generating config file with index.js
    Signed-off-by: Oscar Bernal <oscarbernal93@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 fetchprivate = require('lever-conf/fetchprivate');
+
+const fs = require('fs');
+
 fetchprivate ((err) => {
   conf = require('lever-conf/private');
   console.log(conf.looker)
+  data = JSON.stringify(conf.looker);
+  fs.writeFileSync('config.json', data);
 })
